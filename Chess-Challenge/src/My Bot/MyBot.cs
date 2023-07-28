aa5be7b5e526b95e9742ee28b89057c314b85c39
M. Bamberg <m.a.bamberg.dev@proton.me>
2023-07-28 20:15:50 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -11,7 +11,7 @@ public class MyBot : IChessBot
                               0.0f, 1.0f, 1.2f, 1.6f, 1.5f, 1.4f, 2.0f, 0.0f,   //pawn
                               1.0f, 1.0f, 1.1f, 1.3f, 1.3f, 1.1f, 1.0f, 1.0f,
                               0.9f, 0.9f, 1.1f, 1.0f, 1.0f, 1.1f, 0.9f, 0.9f,   //knight
-                              0.6f, 0.9f, 1.3f, 1.0f, 1.0f, 1.3f, 0.9f, 0.6f,
+                              0.6f, 0.9f, 1.1f, 1.0f, 1.0f, 1.1f, 0.9f, 0.6f,
                               0.7f, 1.0f, 1.2f, 1.4f, 1.4f, 1.2f, 1.0f, 0.7f,   //bishop
                               0.7f, 1.0f, 1.2f, 1.4f, 1.4f, 1.2f, 1.0f, 0.7f,
                               1.0f, 0.9f, 1.2f, 1.2f, 1.2f, 1.0f, 1.0f, 1.3f,   //rook
@@ -24,8 +24,19 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        int Depth = 5;
+
         float boardEvaluation()
         {
+            if(board.IsInCheckmate())
+            {
+                return board.IsWhiteToMove ? float.MinValue : float.MaxValue;
+            }
+            else if (board.IsDraw())
+            {
+                return 0;
+            }
+
             float material = 0;
 
             for (int i = 0; i < 64; i++)
@@ -36,8 +47,8 @@ public class MyBot : IChessBot
                     bool isWhite = piece.IsWhite;
 
                     int whiteMultiplier = isWhite ? 1 : -1;
-                    int row = i / 8;
-                    int column = isWhite ? i % 8 : 7 - (i % 8);
+                    int row = isWhite ? i / 8 : 7 - i / 8;
+                    int column = i % 8;
 
                     material += whiteMultiplier *
                         relativeValue[(16 * ((int)piece.PieceType)) + row] *
@@ -50,8 +61,8 @@ public class MyBot : IChessBot
 
         Move bestMove(out float evaluation, int depth, float alpha = float.MinValue, float beta = float.MaxValue)
         {
-            List<Move> legalMoves = new(board.GetLegalMoves());
-            if (legalMoves.Count == 0)
+            Move[] legalMoves = board.GetLegalMoves();
+            if (legalMoves.Length == 0)
             {
                 if (board.IsInCheckmate())
                 {
@@ -64,15 +75,27 @@ public class MyBot : IChessBot
             }
             if (depth >= 2)
             {
-                for (int i = 0; i < legalMoves.Count; i++)
+                List<Move> legalMovesOrdered = new(legalMoves.Length);
+                List<float> evals = new(legalMoves.Length);
+                for (int i = 0; i < legalMoves.Length; i++)
                 {
-                    if (legalMoves[i].IsCapture || legalMoves[i].IsPromotion || legalMoves[i].IsCastles)
+                    board.MakeMove(legalMoves[i]);
+                    int insertIndex = 0;
+                    float eval = (board.IsWhiteToMove? 1 : -1) * boardEvaluation();
+                    board.UndoMove(legalMoves[i]);
+
+                    for (int j = 0; j < evals.Count; j++)
                     {
-                        Move t = legalMoves[i];
-                        legalMoves.RemoveAt(i);
-                        legalMoves.Insert(i, t);
+                        if(evals[j] > eval)
+                        {
+                            insertIndex = j;
+                            break;
+                        }
                     }
+                    legalMovesOrdered.Insert(insertIndex, legalMoves[i]);
+                    evals.Insert(insertIndex, eval);
                 }
+                legalMoves = legalMovesOrdered.ToArray();
             }
             Move moveToPlay = legalMoves[0];
 
@@ -87,6 +110,7 @@ public class MyBot : IChessBot
                         bestMove(out eval, depth - 1, alpha, beta);
                     else
                         eval = boardEvaluation();
+
                     if (eval > maxEval)
                     {
                         maxEval = eval;
@@ -114,6 +138,7 @@ public class MyBot : IChessBot
                         bestMove(out eval, depth - 1, alpha, beta);
                     else
                         eval = boardEvaluation();
+
                     if (eval < minEval)
                     {
                         minEval = eval;
@@ -139,7 +164,8 @@ public class MyBot : IChessBot
             /*Console.Write("\nSimple evaluation:");
             Console.WriteLine(boardEvaluation() / 100.0f);*/
             float eval;
-            moveToPlay = bestMove(out eval, 5);
+            moveToPlay = bestMove(out eval, Depth);
+
 
             /*Console.WriteLine(eval / 100.0f);
             Console.WriteLine(timer.MillisecondsElapsedThisTurn);
